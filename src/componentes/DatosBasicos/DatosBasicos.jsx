806d65406342566c2f6e77c8d5469d9c5c1741d4
Nickothan <s1nicolasrg@gmail.com>
2021-05-21 07:46:20 -0500

    cambiando nombre de componentes

--- src/componentes/DatosBasicos/DatosBasicos.jsx
+++ src/componentes/DatosBasicos/DatosBasicos.jsx
@@ -2,60 +2,53 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMobileAlt, faEnvelopeSquare, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles.js";
-import {
-    DatosBasicosContainer,
-    DBImg,
-    DatosBasicosContenido,
-    DBTitulos,
-    DBContenInfo,
-    InfoContainer,
-} from "./styles.js";
+import { Container, Img, Content, Titulos, ContenInfo, InfoContainer } from "./styles.js";
 
 import IconHV from "../assets/Captura.png";
 
 const infoContact = [
-    {
-        title: "+57 3209317494",
-        icon: faMobileAlt,
-    },
-    {
-        title: "s1nicolasrg@gmail.com",
-        icon: faEnvelopeSquare,
-    },
-    {
-        title: "Bogotá D.C - Colombia",
-        icon: faMapMarkerAlt,
-    },
+  {
+    title: "+57 3209317494",
+    icon: faMobileAlt,
+  },
+  {
+    title: "s1nicolasrg@gmail.com",
+    icon: faEnvelopeSquare,
+  },
+  {
+    title: "Bogotá D.C - Colombia",
+    icon: faMapMarkerAlt,
+  },
 ];
 
-const InfoDatosBaico = ({ datos: { icon, title } }) => {
-    return (
-        <InfoContainer>
-            <FontAwesomeIcon icon={icon} />
-            <p>{title}</p>
-        </InfoContainer>
-    );
+const Info = ({ datos: { icon, title } }) => {
+  return (
+    <InfoContainer>
+      <FontAwesomeIcon icon={icon} />
+      <p>{title}</p>
+    </InfoContainer>
+  );
 };
 
-export default function Resume() {
-    return (
-        <DatosBasicosContainer>
-            <DBImg>
-                <img src={IconHV} alt="Imagen usuario" />
-            </DBImg>
-            <DatosBasicosContenido>
-                <DBTitulos>
-                    <h1>Sergio Nicolas Ramirez Garcia</h1>
-                    <h2>Developer</h2>
-                </DBTitulos>
+export default function DatosBasicos() {
+  return (
+    <Container>
+      <Img>
+        <img src={IconHV} alt="Imagen usuario" />
+      </Img>
+      <Content>
+        <Titulos>
+          <h1>Sergio Nicolas Ramirez Garcia</h1>
+          <h2>Developer</h2>
+        </Titulos>
 
-                <DBContenInfo>
-                    {infoContact.map((datos) => {
-                        //console.log("datos: ", datos);
-                        return <InfoDatosBaico key={Math.random() * 10} datos={datos} />;
-                    })}
-                </DBContenInfo>
-            </DatosBasicosContenido>
-        </DatosBasicosContainer>
-    );
+        <ContenInfo>
+          {infoContact.map((datos) => {
+            //console.log("datos: ", datos);
+            return <Info key={Math.random() * 10} datos={datos} />;
+          })}
+        </ContenInfo>
+      </Content>
+    </Container>
+  );
 }
--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -3,17 +3,17 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import { FooterContainer } from "./styles.js";
 
-export default function Resume() {
-    return (
-        <FooterContainer>
-            <a href="https://github.com/nickothan">
-                <FontAwesomeIcon icon={faGithub} />
-                <h3>nickothan</h3>
-            </a>
-            <a href="https://www.linkedin.com/in/sergio-ramirez-/">
-                <FontAwesomeIcon icon={faLinkedin} />
-                <h3>Sergio Ramirez</h3>
-            </a>
-        </FooterContainer>
-    );
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <a href="https://github.com/nickothan">
+        <FontAwesomeIcon icon={faGithub} />
+        <h3>nickothan</h3>
+      </a>
+      <a href="https://www.linkedin.com/in/sergio-ramirez-/">
+        <FontAwesomeIcon icon={faLinkedin} />
+        <h3>Sergio Ramirez</h3>
+      </a>
+    </FooterContainer>
+  );
 }
