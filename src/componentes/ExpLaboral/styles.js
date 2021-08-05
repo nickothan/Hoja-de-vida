2608a397159e741c9b0914449c7c2efebc391d8d
Nickothan <38267333+nickothan@users.noreply.github.com>
2021-08-05 13:50:56 -0500

    CN-3 update. CN-5-6-18 finish (#16)

---
+++ src/componentes/ExpLaboral/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro'
+
+export const ExpLabContainer = styled.div`
+  border-bottom: 3px solid #dfdfff;
+  padding: 25px 0;
+`
+export const Date = styled.p`
+  margin-bottom: 5px;
+  font-size: 0.8rem;
+`
+export const Skills = styled(Date)`
+  color: #584cbb;
+`
