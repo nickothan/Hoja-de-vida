8dea0ad2a6f939e4ecf5610f5eee11fb64c45200
Nickothan <s1nicolasrg@gmail.com>
2021-08-05 15:00:40 -0500

    CN-20 textos traducidos

--- src/componentes/Experiencia/Experiencia.jsx
+++ src/componentes/Experiencia/Experiencia.jsx
@@ -2,63 +2,58 @@ import { ExperienciaContent } from './styles.js'
 
 // import data from "./Data.json";
 
-//* CN-7: integrar scss y styled components a css
-//TODO CN-8: Estilos para la sublista
-//* CN-9: integrar jquery, react y typescript a javascript
-//TODO cn-10: agregar stoorybook
-
 export default function Experiencia() {
   return (
     <ExperienciaContent>
-      <h2>Experiencia</h2>
+      <h2>Skills</h2>
       <br />
       <ul>
         <li>
           <div>
             <span>HTML</span>
-            <span>3 años</span>
+            <span>3 years</span>
           </div>
         </li>
         <li>
           <div>
             <span>CSS</span>
-            <span>3 años</span>
+            <span>3 years</span>
           </div>
           <ul>
             <li>
               <span>SCSS</span>
-              <span>2 años</span>
+              <span>2 years</span>
             </li>
             <li>
               <span>Styled Components</span>
-              <span>1 año</span>
+              <span>1 year</span>
             </li>
           </ul>
         </li>
         <li>
           <div>
             <span>JavaScript</span>
-            <span>3 años</span>
+            <span>3 years</span>
           </div>
           <ul>
             <li>
               <span>Jquery</span>
-              <span>2 años</span>
+              <span>2 years</span>
             </li>
             <li>
               <span>React</span>
-              <span>1 año</span>
+              <span>1 year</span>
             </li>
             <li>
               <span>TypeScript</span>
-              <span>1 año</span>
+              <span>1 year</span>
             </li>
           </ul>
         </li>
         <li>
           <div>
             <span>Git</span>
-            <span>2 años</span>
+            <span>2 years</span>
           </div>
         </li>
       </ul>
@@ -71,7 +66,7 @@ export default function Experiencia() {
               return (
                 <li key={Math.random() * 10}>
                   <span>{item}</span>
-                  <span>{` ${time} ${time === 1 ? "año" : "años"}`}</span>
+                  <span>{` ${time} ${time === 1 ? "year" : "years"}`}</span>
                 </li>
               );
             })}
