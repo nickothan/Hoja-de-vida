d2331c51dd893eb4fab686447f0d23609668236f
Nickothan <38267333+nickothan@users.noreply.github.com>
2021-08-06 08:46:55 -0500

    [CN-19] Translate (#23)
    * CN-20 textos traducidos
    
    * Update

--- src/componentes/Proyectos/Proyectos.jsx
+++ src/componentes/Proyectos/Proyectos.jsx
@@ -7,7 +7,9 @@ export default function Proyectos() {
     {
       title: 'Tic-Tac-Toe',
       desctipcion:
-        'Tic-tac-toe game elaborated in react.js for two players, X and O, who take turns marking the spaces in a 3 × 3 grid. The player who manages to place three respective marks in a horizontal, vertical or diagonal row wins the game.',
+
+        'Tic-tac-toe is a game builded with react.js where two players can play using X and O, each turn have to mark with the icon in the space on a 3 × 3 grid. The player who has three respective marks in a horizontal, vertical or diagonal line wins the game.',
+
       link: 'https://tic-tac-toe-nico.netlify.app/'
     },
     {
--- src/componentes/Resumen/Resumen.jsx
+++ src/componentes/Resumen/Resumen.jsx
@@ -8,10 +8,12 @@ export default function Resumen() {
       <br />
       <p>
         I am an open-minded developer, passionate about designing, layout, and
-        prototype different solutions. He dedicated most of my Front-end career,
-        I'm working with react, I have experience developing in java and PHP.
-        with 4 years of development experience Web. I am ready for new
-        challenges. I love working with greats people, inspire others and be
+
+        prototype different solutions. I dedicated most of the time in
+        Front-end, I'm working with react, I have experience developing in java
+        and PHP. with 3 years of development carrer. I am ready for new
+        challenges. I love working with great people, inspire others and be
+
         inspired.
       </p>
     </ResumeContainer>
