e289b7472184f2f09c93c6c5ab5b85babfe75b15
Nickothan <s1nicolasrg@gmail.com>
2021-08-10 11:05:53 -0500

    CN-22 update, CN-23-24 finish

--- src/componentes/Proyectos/styles.js
+++ src/componentes/Proyectos/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 export const ExperienciaContent = styled.div`
   padding-top: 10px;
@@ -21,7 +21,10 @@ export const LinkContainer = styled.div`
 
   cursor: pointer;
 
+  animation-timing-function: linear;
+
   &:hover {
+    font-weight: 600;
     color: #6658dd;
     background-color: #6658dd21;
   }
